77e1dc73a5c32d8c92219dd10879211f784d36ae
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2018-04-01 16:24:40 -0700

    Python scraping script

--- res_script.py
+++ res_script.py
@@ -5,13 +5,25 @@ import requests
 import re
 from collections import defaultdict
 import time
+import sys
+import urllib
+from pymongo import MongoClient
+import config
 
+client = MongoClient(config.MONGODB_URI, connectTimeoutMS=30000)
+db = client[config.DATABASE]
+upper = db.upper_pines
+lower =	db.lower_pines
+crane = db.crane_flat
+north =	db.north_pines
+waw = db.wawona
+hodg = db.hodgdon
 #20 weeks (2 * 14 days)
 def get_reservation_data(site_dict, url1, num_of_sites):
 	now = datetime.datetime.now()
 	url2 = '&sitepage=true&startIdx='
 	session = requests.Session()
-	for i in range(10):
+	for i in range(1):
 		idx = 0
 		nowt = now.strftime("%m/%d/%Y")
 		url3 = url1 + str(nowt) + url2
@@ -21,7 +33,6 @@ def get_reservation_data(site_dict, url1, num_of_sites):
 			print(complete_url)
 			upper_pines_this_week = session.get(complete_url)
 			txt = upper_pines_this_week.text
-
 			res_body = BeautifulSoup(txt, "html.parser").find_all('tbody')
 			rows = BeautifulSoup(str(res_body), "html.parser").find_all('tr')
 			#loop through all rows in availability table
@@ -36,7 +47,6 @@ def get_reservation_data(site_dict, url1, num_of_sites):
 					val = txt[0]
 					try:
 						num = int(txt[3])
-						print(num)
 					except IndexError:
 						pass
 					except ValueError:
@@ -44,10 +54,11 @@ def get_reservation_data(site_dict, url1, num_of_sites):
 					#if the first cell holds an int, we know the following tds are valid reservations for that site
 					if num != 0:
 						if val == 'X' or val == 'R' or val =='A' or val == 'N' or val == 'W':
-							site_dict[num].append(val)
+							site_dict[str(num)].append(val)
 			print(now.strftime("%m/%d/%y"))
 			print(idx)
-			idx += 25	
+			idx += 25
+			time.sleep(.25)
 		now = now + timedelta(days=14)
 
 def main():
@@ -72,25 +83,33 @@ def main():
 	np_sites = 79
 	wa_sites = 97
 	ho_sites = 103
+
+	#clear database
+	upper.delete_many({})
+	lower.delete_many({})
+	crane.delete_many({})
+	north.delete_many({})
+	waw.delete_many({})
+	hodg.delete_many({})
 	#Populate each Dictionary
-	get_reservation_data(upper_pines, upper_pines_base_url, up_sites)
-	get_reservation_data(lower_pines, lower_pines_base_url, lp_sites)
+	get_reservation_data(upper_pines, upper_pines_base_url, up_sites)	
 	get_reservation_data(crane_flat, crane_flat_base_url, cf_sites)
 	get_reservation_data(north_pines, north_pines_base_url, np_sites)
 	get_reservation_data(wawona, wawona_base_url, wa_sites)
 	get_reservation_data(hodgdon, hodgdon_base_url, ho_sites)
+	get_reservation_data(lower_pines, lower_pines_base_url, lp_sites)
 	for i in upper_pines:
-		print("Upper Pines Site: ", i, upper_pines[i])
+		upper.insert_one({ "_id" : i, "availability" : upper_pines[i]})
 	for i in lower_pines:
-		print("Lower Pines Site: ", i, lower_pines[i])
+		lower.insert_one({ "_id" : i, "availability" : lower_pines[i]})
 	for i in crane_flat:
-		print("Crane Flat Site: ", i, crane_flat[i])
+		crane.insert_one({ "_id" : i, "availability" : crane_flat[i]})
 	for i in north_pines:
-		print("North Pines Site: ", i, north_pines[i])
+		north.insert_one({ "_id" : i, "availability" : north_pines[i]})
 	for i in wawona:
-		print("Wawona Site: ", i, wawona[i])
+		waw.insert_one({ "_id" : i, "availability" : wawona[i]})
 	for i in hodgdon:
-		print("Hodgdon Site: ", i, hodgdon[i])
+		hodg.insert_one({ "_id" : i, "availability" : hodgdon[i]})
 
 main()
 
